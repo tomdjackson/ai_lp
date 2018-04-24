cd8cabb466669e268bcc7a8de5acd0397fd2028e
tomdjackson <tj15918@bristol.ac.uk>
2018-04-24 23:23:29 +0100

    lol

--- assignment2_21611.pl
+++ assignment2_21611.pl
@@ -10,33 +10,51 @@ candidate_number(21611).
 solve_task(Task,Cost):-
   my_agent(Agent),
   query_world( agent_current_position, [Agent,P] ),
-  solve_task_a_star(Task,[[c(, 0, P),P]], R,Cost,_NewPos),!,  % prune choice point for efficiency
+  solve_task_a_star(Task,[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
   reverse(R,[_Init|Path]),
   query_world( agent_do_moves, [Agent,Path] ).
 
-%%%%%%%%%% A* Search %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-solve_task_a_star(Task, [Current|_], R, Costs, NewPos):-
-  Current = [Current1|RPath],
-  Costs = [cost(Cost), depth(Depth)],
-  achieved(Task, Current, RPath, Cost, NewPos),
-solve_task_a_star(Task, Agenda, RPath, Cost, NewPos):-
-  Agenda = [Current|Tail],
-  Cost = [c(_, G, P1)|RPath],
-  Task = go(p(X,Y)),
-  write("hello"),
-  setof(Child, create_child(Child, p(X,Y), Task, RPath, G), Children),
-  append(Agenda, Children, NewAgenda),
-  solve_task_a_star(Task, NewAgenda, RPath, Cost, NewPos).
+solve_task_a_star(Task,[Current|_],R,Costs,NewPos,_) :-
+  Current = [c(_,G,P)|RPath],
+  Costs = [cost(Cost), depth(G)],
+  achieved(Task, [c(G,P)|RPath], R, Cost, NewPos).
+solve_task_a_star(go(Goal),[Current|Agenda],RR,Cost,NewPos,Visited) :-
+  Current = [c(_,G,P)|RPath],
+  (
+    setof(Child, a_star_child(Goal, P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda);
+    NewAgenda = Agenda
+  ),
+  remove_visited(NewAgenda, Visited, FinalAgenda),
+  solve_task_a_star(go(Goal),FinalAgenda,RR,Cost,NewPos,[P|Visited]).
+solve_task_a_star(Task,[Current|Agenda],RR,Cost,NewPos, Visited) :-
+    Current = [c(_,G,P)|RPath],
+    (
+      findall(Child, breadth_child(P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda);
+      NewAgenda = Agenda
+    ),
+    remove_visited(NewAgenda, Visited, FinalAgenda),
+    solve_task_a_star(Task,FinalAgenda,RR,Cost,NewPos,[P|Visited]).
 
-create_child(Child, P, Task, RPath, G):-
-  write(P),
-  search(P,P1,P1,_),
-  \+memberchk(P1, RPath),
-  map_distance(P, P1, H),
-  write(H),
+remove_visited(Agenda, [], Agenda).
+remove_visited(Agenda, [P|Tail], NewAgenda) :-
+    delete(Agenda, [c(_,_,P)|_], NewAgenda),
+    remove_visited(NewAgenda, Tail, _).
+
+a_star_child(Goal, P, RPath, G, Child, Visited) :-
+  search(P, P1, R, _),
+  \+ memberchk(R, RPath),
+  \+ memberchk(P1, Visited),
+  map_distance(P1, Goal, H),
   G1 is G+1,
-  F is G+H,
-  Child = [c(F, G1, P1)|RPath].
+  F is G1+H,
+  Child = [c(F, G1, P1), P1|RPath].
+
+breadth_child(P, RPath, G, Child, Visited) :-
+  search(P, P1, R, _),
+  \+ memberchk(R, RPath),
+  \+ memberchk(P1, Visited),
+  G1 is G+1,
+  Child = [c(G1, G1, P1), P1|RPath].
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% backtracking depth-first search, needs to be changed to agenda-based A*
