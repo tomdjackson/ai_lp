0b872799d93f597afa291660bcff012019387843
tomdjackson <tj15918@bristol.ac.uk>
2018-04-26 17:29:18 +0100

    finished part 3

--- assignment2_21611.pl
+++ assignment2_21611.pl
@@ -7,12 +7,52 @@ candidate_number(21611).
 %   reverse(R,[_Init|Path]),
 %   query_world( agent_do_moves, [Agent,Path] ).
 
-solve_task(Task,Cost):-
+solve_task(go(X),Cost):-
   my_agent(Agent),
+  check_energy(Agent),
+  write("GOING TO "),
+  X = p(I,J),
+  write("("), write(I), write(","), write(J), write(")"),nl,
   query_world( agent_current_position, [Agent,P] ),
-  solve_task_a_star(Task,[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
+  solve_task_a_star(go(X),[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
   reverse(R,[_Init|Path]),
   query_world( agent_do_moves, [Agent,Path] ).
+
+solve_task(find(o(X)),Cost):-
+  my_agent(Agent),
+  check_energy(Agent),
+  write("GOING TO ORACLE"),nl,
+  query_world( agent_current_position, [Agent,P] ),
+  solve_task_breadth(find(o(X)),[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
+  reverse(R,[_Init|Path]),
+  query_world( agent_do_moves, [Agent,Path] ).
+
+solve_task(find_next_oracle(o(X)),Cost):-
+  my_agent(Agent),
+  check_energy(Agent),
+  write("GOING TO NEXT ORACLE"),nl,
+  query_world( agent_current_position, [Agent,P] ),
+  solve_task_breadth(find_next_oracle(o(X)),[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
+  reverse(R,[_Init|Path]),
+  query_world( agent_do_moves, [Agent,Path] ).
+
+solve_task(find(c(X)),Cost):-
+  my_agent(Agent),
+  write("GOING TO CHARGING STATION"),nl,
+  query_world( agent_current_position, [Agent,P] ),
+  solve_task_breadth(find(c(X)),[[c(0,0,P),P]],R,Cost,_NewPos,[]),!,
+  reverse(R,[_Init|Path]),
+  query_world( agent_do_moves, [Agent,Path] ),
+  query_world( agent_topup_energy, [Agent, c(X)]).
+
+check_energy(Agent):-
+  write("CHECKING ENERGY"),nl,
+  query_world(agent_current_energy, [Agent, Energy]),
+  write("ENERGY = "),
+  write(Energy),nl,
+  ( Energy > 50 -> true
+  ; otherwise -> solve_task(find(c(_)), A)
+  ).
 
 solve_task_a_star(Task,[Current|_],R,Costs,NewPos,_) :-
   Current = [c(_,G,P)|RPath],
@@ -20,25 +60,16 @@ solve_task_a_star(Task,[Current|_],R,Costs,NewPos,_) :-
   achieved(Task, [c(G,P)|RPath], R, Cost, NewPos).
 solve_task_a_star(go(Goal),[Current|Agenda],RR,Cost,NewPos,Visited) :-
   Current = [c(_,G,P)|RPath],
-  (
-    setof(Child, a_star_child(Goal, P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda);
-    NewAgenda = Agenda
+  ( setof(Child, a_star_child(Goal, P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda)
+  ; NewAgenda = Agenda
   ),
-  remove_visited(NewAgenda, Visited, FinalAgenda),
+  remove_visited_from_agenda(NewAgenda, Visited, FinalAgenda),
   solve_task_a_star(go(Goal),FinalAgenda,RR,Cost,NewPos,[P|Visited]).
-solve_task_a_star(Task,[Current|Agenda],RR,Cost,NewPos, Visited) :-
-    Current = [c(_,G,P)|RPath],
-    (
-      findall(Child, breadth_child(P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda);
-      NewAgenda = Agenda
-    ),
-    remove_visited(NewAgenda, Visited, FinalAgenda),
-    solve_task_a_star(Task,FinalAgenda,RR,Cost,NewPos,[P|Visited]).
 
-remove_visited(Agenda, [], Agenda).
-remove_visited(Agenda, [P|Tail], NewAgenda) :-
+remove_visited_from_agenda(Agenda, [], Agenda).
+remove_visited_from_agenda(Agenda, [P|Tail], NewAgenda) :-
     delete(Agenda, [c(_,_,P)|_], NewAgenda),
-    remove_visited(NewAgenda, Tail, _).
+    remove_visited_from_agenda(NewAgenda, Tail, _).
 
 a_star_child(Goal, P, RPath, G, Child, Visited) :-
   search(P, P1, R, _),
@@ -48,6 +79,18 @@ a_star_child(Goal, P, RPath, G, Child, Visited) :-
   G1 is G+1,
   F is G1+H,
   Child = [c(F, G1, P1), P1|RPath].
+
+solve_task_breadth(Task, [Current|_], R, Costs, NewPos, _):-
+  Current = [c(_,G,P)|RPath],
+  Costs = [cost(Cost), depth(G)],
+  achieved(Task, [c(G,P)|RPath], R, Cost, NewPos).
+solve_task_breadth(Task, [Current|Agenda], RR, Cost, NewPos, Visited):-
+  Current = [c(_,G,P)|RPath],
+  (setof(Child, breadth_child(P, RPath, G, Child, Visited), Children) -> append(Agenda, Children, NewAgenda)
+  ; NewAgenda = Agenda
+  ),
+  remove_visited_from_agenda(NewAgenda, Visited, FinalAgenda),
+  solve_task_breadth(Task,FinalAgenda,RR,Cost,NewPos,[P|Visited]).
 
 breadth_child(P, RPath, G, Child, Visited) :-
   search(P, P1, R, _),
@@ -72,6 +115,13 @@ achieved(go(Exit),Current,RPath,Cost,NewPos) :-
   Current = [c(Cost,NewPos)|RPath],
   ( Exit=none -> true
   ; otherwise -> RPath = [Exit|_]
+  ).
+achieved(find_next_oracle(O), Current, RPath, Cost, NewPos) :-
+  Current = [c(Cost, NewPos)|RPath],
+  my_agent(Agent),
+  ( O=none -> true
+  ; otherwise -> RPath = [Last|_], map_adjacent(Last,_,O), write("ORACLE FOUND, I = "), write(O), nl,
+  \+ query_world(agent_check_oracle, [Agent, O ])
   ).
 achieved(find(O),Current,RPath,Cost,NewPos) :-
   Current = [c(Cost,NewPos)|RPath],
--- assignment2_wp_21611.pl
+++ assignment2_wp_21611.pl
@@ -11,9 +11,6 @@ find_identity_2(A):-
   findall(Actor, actor(Actor), Actors),
   find_actor_identity(A, Actors).
 
-find_identity_o(A):-
-  A='Not yet implemented'.
-
 find_actor_identity(A, Actors) :-
   agent_ask_oracle(oscar, o(1), link, L),
   include(link_on_page(L), Actors, NewActors),
@@ -29,3 +26,19 @@ link_on_page(Link,A):-
 links_on_page(A, Links):-
   actor(A),
   setof(Link, (link(Link), wp(A,WT),wt_link(WT,Link)), Links).
+
+%%%%%%%%%%%%%%%%%%%%%%% PART 3 %%%%%%%%%%%%%%%%%%%%%%%
+
+find_identity_o(A):-
+  findall(Actor, actor(Actor), Actors),
+  find_actor_identity_o(A, Actors).
+
+find_actor_identity_o(A, Actors) :-
+  solve_task(find_next_oracle(o(X)), _),
+  my_agent(Agent),
+  query_world( agent_ask_oracle, [Agent, o(X), link, L]),
+  include(link_on_page(L), Actors, NewActors),
+  length(NewActors, Length),
+  ( Length = 1 -> nth0(0, NewActors, A)
+  ; find_actor_identity_o(A, NewActors)
+  ).
